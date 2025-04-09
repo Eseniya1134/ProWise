cb687b45a154e02174c7e9917faeb090c9c90015
eseniya1134 <shakirova.esya23@mail.ru>
2025-04-09 18:29:01 +0300

    красивая скрутка-раскрутка лк

--- app/src/main/java/com/example/ourpro/bottomnav/orders/OrdersFragment.java
+++ app/src/main/java/com/example/ourpro/bottomnav/orders/OrdersFragment.java
@@ -9,7 +9,8 @@ import androidx.fragment.app.Fragment;
 
 import com.example.ourpro.R;
 
-public class OrdersFragment extends Fragment {
+public class
+OrdersFragment extends Fragment {
     public OrdersFragment() {
         super(R.layout.fragment_orders);
     }
