6c3ae972c785da4500ad883dfc4d98c132ca14d5
eseniya1134 <shakirova.esya23@mail.ru>
2025-04-14 23:50:29 +0300

    поиск ника

--- app/src/main/java/com/example/ourpro/bottomnav/catalog/CatalogFragment.java
+++ app/src/main/java/com/example/ourpro/bottomnav/catalog/CatalogFragment.java
@@ -8,14 +8,43 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.ourpro.R;
+import com.example.ourpro.databinding.FragmentCatalogBinding;
 
 public class CatalogFragment extends Fragment {
     public CatalogFragment() {
         super(R.layout.fragment_catalog);
     }
 
+    private FragmentCatalogBinding binding;
+    private UserAdapter userAdapter;
+    private DatabaseHelper databaseHelper;
+
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentCatalogBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        userAdapter = new UserAdapter();
+        databaseHelper = new DatabaseHelper(getContext());
+
+        binding.recyclerViewResults.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerViewResults.setAdapter(userAdapter);
+
+        binding.buttonSearch.setOnClickListener(v -> {
+            String nickname = binding.editTextNickname.getText().toString();
+            List<User> users = databaseHelper.getUsersByNickname(nickname);
+            userAdapter.setUserList(users);
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
+
+
