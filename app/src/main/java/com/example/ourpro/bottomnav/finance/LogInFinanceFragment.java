05197f66d9c9e01d454755ea9606f99cc87f9ea8
eseniya1134 <shakirova.esya23@mail.ru>
2025-05-07 23:20:01 +0300

    финансовый отдел: подключение к бд, регистрация и вход через пин в WiseCash

--- app/src/main/java/com/example/ourpro/bottomnav/finance/LogInFinanceFragment.java
+++ app/src/main/java/com/example/ourpro/bottomnav/finance/LogInFinanceFragment.java
@@ -29,6 +29,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,32 +38,56 @@ public class LogInFinanceFragment extends Fragment {
 
     private FragmentLogInFinanceBinding binding;
     private ImageView[] pinCircles = new ImageView[4];
+
+    private String confirmedPin;
+
+    private String flag = "0";
     private String pin;
+
+    private DatabaseReference rtdb;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentLogInFinanceBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
+        rtdb = FirebaseDatabase.getInstance("https://prowise-de1d0-default-rtdb.europe-west1.firebasedatabase.app").getReference();
+        DatabaseReference refUser = rtdb.child("Users").child(userId);
+        refUser.child("WiseCash").get().addOnSuccessListener(snapshot -> {
+            flag = snapshot.getValue(String.class);
+            Log.d(TAG, "Флаг из базы: " + flag);
 
-        // Переход от 0 → 1
-        binding.btnCreate.setOnClickListener(v -> {
-            if (binding.viewFlipper.getDisplayedChild() == 0) {
-                binding.viewFlipper.showNext();
+            if ("1".equals(flag)) {
+                binding.viewFlipper.setDisplayedChild(5);
+                signInCash();
+            } else {
+                // Переход от 0 → 1
+                binding.btnCreate.setOnClickListener(v -> {
+                    if (binding.viewFlipper.getDisplayedChild() == 0) {
+                        binding.viewFlipper.showNext();
+                    }
+                });
+
+                // Проверка имени и переход 1 → 2 → 3
+                binding.btnCheck.setOnClickListener(v -> flagInFbdb());
+
+                // Переход на финальный экран
+                binding.btnNext.setOnClickListener(v -> {
+                    if (binding.viewFlipper.getDisplayedChild() == 4) {
+                        addCash();
+                        FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+                        ft.replace(R.id.menu_fr, new FinanceFragment());
+                        ft.addToBackStack(null);
+                        ft.commit();
+                    }
+                });
             }
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "Ошибка чтения флага: " + e.getMessage());
         });
 
-        // Проверка имени и переход 1 → 2 → 3
-        binding.btnCheck.setOnClickListener(v -> flagInFbdb());
 
-        // Переход на финальный экран (если вдруг потребуется)
-        binding.btnNext.setOnClickListener(v -> {
-            if (binding.viewFlipper.getDisplayedChild() == 4) {
-                FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-                ft.replace(R.id.menu_fr, new FinanceFragment());
-                ft.addToBackStack(null); // Добавляем в back stack
-                ft.commit();
-            }
-        });
 
         return view;
     }
@@ -82,19 +107,11 @@ public class LogInFinanceFragment extends Fragment {
                     String inputUsername = binding.usernameGet.getText().toString().trim().toLowerCase();
 
                     if (presentUsername != null && inputUsername.equals(presentUsername.trim().toLowerCase())) {
-                        Map<String, Object> userUpdates = new HashMap<>();
-                        userUpdates.put("WiseCash", "true");
-
-                        databaseReference.child(userId).updateChildren(userUpdates).addOnSuccessListener(aVoid -> {
-                            Log.d(TAG, "Кошелек создан в Firebase");
-
-                            if (binding.viewFlipper.getDisplayedChild() == 1) {
-                                binding.viewFlipper.showNext();
-
-                                // Ждём перехода к экрану и инициализируем PIN
-                                new Handler(Looper.getMainLooper()).postDelayed(this::initPinInput, 200);
-                            }
-                        }).addOnFailureListener(e -> Log.e(TAG, "Ошибка: " + e.getMessage()));
+                        if (binding.viewFlipper.getDisplayedChild() == 1) {
+                            binding.viewFlipper.showNext();
+                            // Ждём перехода к экрану и инициализируем PIN
+                            new Handler(Looper.getMainLooper()).postDelayed(this::initPinInput, 200);
+                        }
                     } else {
                         Toast.makeText(requireContext(), "Имя пользователя введено неверно!", Toast.LENGTH_SHORT).show();
                     }
@@ -180,6 +197,8 @@ public class LogInFinanceFragment extends Fragment {
             String pinCode = binding.pinEditText2.getText().toString();
             if (pinCode.length() == 4) {
                 if (pinCode.equals(pin)) {
+                    confirmedPin = pinCode;
+
                     Toast.makeText(getContext(), "PIN совпадает", Toast.LENGTH_SHORT).show();
                     if (binding.viewFlipper.getDisplayedChild() == 3) {
                         binding.viewFlipper.showNext();
@@ -193,6 +212,68 @@ public class LogInFinanceFragment extends Fragment {
         });
     }
 
+    /// ШАГ 6 - Введение PIN-кода при входе
+    private void signInCash(){
+        pinCircles[0] = binding.pin3Circle1;
+        pinCircles[1] = binding.pin3Circle2;
+        pinCircles[2] = binding.pin3Circle3;
+        pinCircles[3] = binding.pin3Circle4;
+
+        binding.pinEditText3.setVisibility(View.VISIBLE);
+        binding.pinEditText3.setCursorVisible(false);
+        binding.pinEditText3.requestFocus();
+
+        InputMethodManager imm = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.showSoftInput(binding.pinEditText2, InputMethodManager.SHOW_IMPLICIT);
+        }
+
+        binding.pinEditText3.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updatePinCircles(s.length());
+            }
+        });
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
+        rtdb = FirebaseDatabase.getInstance("https://prowise-de1d0-default-rtdb.europe-west1.firebasedatabase.app").getReference();
+        DatabaseReference refCash = rtdb.child("WiseCash").child(userId);
+
+        refCash.child("pin").get().addOnSuccessListener(dataSnapshot -> {
+            if (dataSnapshot.exists()) {
+                String pinUser = dataSnapshot.getValue(String.class);
+
+                binding.btnSignIn.setOnClickListener(v -> {
+                    String pinCode = binding.pinEditText3.getText().toString();
+                    Log.e(TAG, "Введённый PIN: " + pinCode);
+                    if (pinCode.length() == 4) {
+                        if (pinCode.equals(pinUser)) {
+                            if (binding.viewFlipper.getDisplayedChild() == 5) {
+                                FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+                                ft.replace(R.id.menu_fr, new FinanceFragment());
+                                ft.addToBackStack(null);
+                                ft.commit();
+                            }
+                        } else {
+                            Toast.makeText(requireContext(), "PIN-код не совпадает", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(requireContext(), "Введите 4 цифры", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            } else {
+                Toast.makeText(requireContext(), "PIN-код не найден в базе", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(requireContext(), "Ошибка чтения PIN: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
     /// Обновление кружочков
     private void updatePinCircles(int length) {
         for (int i = 0; i < 4; i++) {
@@ -202,6 +283,32 @@ public class LogInFinanceFragment extends Fragment {
                 pinCircles[i].setImageResource(R.drawable.pin_circle_empty);
             }
         }
+    }
+
+    ///ДОБАВЛЕНИЕ В БД ИНФОРМАЦИИ О РЕГИСТРАЦИИ В WiseCash
+    private void addCash(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
+        rtdb = FirebaseDatabase.getInstance("https://prowise-de1d0-default-rtdb.europe-west1.firebasedatabase.app").getReference();
+        DatabaseReference refUser = rtdb.child("Users").child(userId);
+        refUser.get().addOnSuccessListener(userBase -> {
+            if (userBase.exists() && userBase.getValue() != null) {
+                Map<String, Object> userUpdates = new HashMap<>();
+                userUpdates.put("WiseCash", "1");
+                refUser.updateChildren(userUpdates)
+                        .addOnSuccessListener(aVoid -> Log.d(TAG, "Кошелек создан в Firebase"))
+                        .addOnFailureListener(e -> Log.e(TAG, "Ошибка: " + e.getMessage()));
+            }
+        }).addOnFailureListener(e -> Log.e(TAG, "Ошибка загрузки пользователя: " + e.getMessage()));
+
+        DatabaseReference refCash = rtdb.child("WiseCash").child(userId);
+        Map<String, Object> userUpdates = new HashMap<>();
+        userUpdates.put("pin", confirmedPin);
+
+        refCash.updateChildren(userUpdates)
+                .addOnSuccessListener(aVoid -> Log.d(TAG, "PIN сохранён в WiseCash"))
+                .addOnFailureListener(e -> Log.e(TAG, "Ошибка сохранения PIN: " + e.getMessage()));
+
     }
 
     @Override
